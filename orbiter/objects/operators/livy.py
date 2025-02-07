a24047d9204223c0dffecb674504937ec60d179f
fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>
2025-02-06 21:04:57 -0500

    fix: add docs

--- orbiter/objects/operators/livy.py
+++ orbiter/objects/operators/livy.py
@@ -3,6 +3,30 @@ from orbiter.objects.task import OrbiterOperator, RenderAttributes
 
 
 class OrbiterLivyOperator(OrbiterOperator):
+    """
+    An Airflow
+    [LivyOperator](https://registry.astronomer.io/providers/apache-airflow-providers-apache-livy/versions/3.9.1/modules/LivyOperator).
+    Used to submit Spark Jobs.
+
+    ```pycon
+    >>> OrbiterLivyOperator(task_id="foo", file="/a/b/c.jar")
+    foo_task = LivyOperator(task_id='foo', file='/a/b/c.jar')
+
+    ```
+    :param task_id: The `task_id` for the operator
+    :type task_id: str
+    :param **kwargs: Extra arguments to pass to the operator
+    :param **OrbiterBase: [OrbiterBase][orbiter.objects.OrbiterBase] inherited properties
+    """  # noqa: E501
+
+    __mermaid__ = """
+    --8<-- [start:mermaid-props]
+    operator = "EmailOperator"
+    task_id: str
+    kwargs: Any
+    --8<-- [end:mermaid-props]
+    """
+
     imports: ImportList = [
         OrbiterRequirement(
             package="apache-airflow-providers-apache-livy",
@@ -14,3 +38,9 @@ class OrbiterLivyOperator(OrbiterOperator):
 
     # noinspection Pydantic
     render_attributes: RenderAttributes = OrbiterOperator.render_attributes
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
