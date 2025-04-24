b9cdfbcd78133f0f60e02e5c7d63c4a4a9b041c0
fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>
2025-04-24 15:31:13 -0400

    doc: update winrm docs

--- orbiter/objects/operators/win_rm.py
+++ orbiter/objects/operators/win_rm.py
@@ -1,8 +1,40 @@
+from __future__ import annotations
+
 from orbiter.objects import ImportList, OrbiterRequirement
 from orbiter.objects.task import OrbiterOperator, RenderAttributes
 
 
 class OrbiterWinRMOperator(OrbiterOperator):
+    """
+    An Airflow
+    [WinRMOperator](https://registry.astronomer.io/providers/apache-airflow-providers-microsoft-winrm/versions/latest/modules/WinRMOperator).
+    Used to run commands on remote Window Servers.
+
+    ```pycon
+    >>> OrbiterWinRMOperator(task_id="foo", command="echo hello")
+    foo_task = WinRMOperator(task_id='foo', ssh_conn_id='winrm_default', command="echo hello")
+
+    ```
+    :param task_id: The `task_id` for the operator
+    :type task_id: str
+    :param ssh_conn_id: The SSH connection to use. (Note: use the `**conn_id(...)` utility function)
+    :type ssh_conn_id: str
+    :param command: The command to execute
+    :type command: str
+    :param **kwargs: Extra arguments to pass to the operator
+    :param **OrbiterBase: [OrbiterBase][orbiter.objects.OrbiterBase] inherited properties
+    """  # noqa: E501
+
+    __mermaid__ = """
+     --8<-- [start:mermaid-props]
+     operator = "WinRMOperator"
+     task_id: str
+     ssh_conn_id: str
+     command: str
+     kwargs: Any
+     --8<-- [end:mermaid-props]
+     """
+
     imports: ImportList = [
         OrbiterRequirement(
             package="apache-airflow-providers-microsoft-winrm",
