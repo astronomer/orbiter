de72c8f31bfcb19d76f2c3bf84da6e0ee2c1eeb5
fritz-astronomer <80706212+fritz-astronomer@users.noreply.github.com>
2026-01-16 17:06:02 -0500

    fix(objects): UnmappedOp error - kwargs only for init

--- orbiter/assets/operators/unmapped_src.py
+++ orbiter/assets/operators/unmapped_src.py
@@ -3,5 +3,5 @@ from airflow.operators.empty import EmptyOperator
 
 class UnmappedOperator(EmptyOperator):
     def __init__(self, task_id: str, source: str, **kwargs):
-        super().__init__(task_id, **kwargs)
+        super().__init__(task_id=task_id, **kwargs)
         self.source = source
